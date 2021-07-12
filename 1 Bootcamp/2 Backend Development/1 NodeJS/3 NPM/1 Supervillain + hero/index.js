02e303b3cca7d1a1d6df4379d67cad29d9ae1646
thisisankit27 <thisisankitsrivastava@gmail.com>
2021-07-12 17:30:44 +0530

    12.07.21
    Signed-off-by: thisisankit27 <thisisankitsrivastava@gmail.com>

---
+++ 1 Bootcamp/2 Backend Development/1 NodeJS/3 NPM/1 Supervillain + hero/index.js
@@ -0,0 +1,7 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var name = superheroes.random();
+var vName = supervillains.random();
+
+console.log(vName);
---
+++ 1 Bootcamp/2 Backend Development/1 NodeJS/3 NPM/2 Retry/index.js
@@ -0,0 +1,4 @@
+//const supervillains = require('supervillains');
+const catNames = require('cat-names');
+
+console.log(catNames.random());
